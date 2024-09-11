5a931b894aad4331f15c591b8be844085a047db0
zhaozhenyu1111 <contact@zhaozhenyu.cn>
2024-09-11 11:36:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const str = '我爱你'
+</script>
+
+<template>
+  <div class="box">
+    initial
+    <h1>App跟组件</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+
+  h1 {
+    color: white;
+  }
+}
+</style>
